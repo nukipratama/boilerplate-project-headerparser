0dd7410e7759170d21bb4d4aa05fe603f877d46d
Nuki Pratama <nukipratamaa@gmail.com>
2022-03-13 19:25:31 +0700

    solution

--- server.js
+++ server.js
@@ -6,10 +6,11 @@ require('dotenv').config();
 var express = require('express');
 var app = express();
 
+app.set('trust proxy', true);
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -22,9 +23,16 @@ app.get("/", function (req, res) {
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  res.json({ greeting: 'hello API' });
 });
 
+app.get("/api/whoami", function (req, res) {
+  res.json({
+    ipaddress: req.ip,
+    language: req.headers["accept-language"],
+    software: req.headers["user-agent"]
+  });
+});
 
 
 // listen for requests :)
